b50e3bd8e8a59e1a81b0bffd7eda63bdd40b1e63
LeiwenPro <240370818@qq.com>
2026-07-18 11:44:40 +0800

    refactor: finish admin component ownership migration

--- apps/web/src/components/admin/AdminLogout.tsx
+++ apps/web/src/components/admin/AdminLogout.tsx
@@ -1,17 +1,1 @@
-'use client'
-
-export function AdminLogout() {
-  return (
-    <button
-      type="button"
-      className="hover:underline"
-      onClick={() => {
-        void fetch('/api/admin/unlock', { method: 'DELETE' }).then(() => {
-          window.location.href = '/admin/gate'
-        })
-      }}
-    >
-      退出密钥
-    </button>
-  )
-}
+export { AdminLogout } from '../../../../admin/components/AdminLogout'
